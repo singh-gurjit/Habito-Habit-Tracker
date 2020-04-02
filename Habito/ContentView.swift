444eef5e2d89826cfb8b520597ad39889ff520ba
Gurjit Singh <gurjit.singh.engineer@gmail.com>
2020-04-02 21:36:19 +0100

    Initial Commit

---
+++ Habito/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Habito
+//
+//  Created by Gurjit Singh on 02/04/20.
+//  Copyright © 2020 Gurjit Singh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
